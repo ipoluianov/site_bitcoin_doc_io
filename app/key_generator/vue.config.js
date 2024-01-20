91ca07ae2f582e1768b8bd85a39bcbfd36269059
Ivan Poluianov <ivan.poluianov@gmail.com>
2024-01-20 10:43:33 +0100

    app

--- app/key_generator/vue.config.js
+++ app/key_generator/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig(
 {
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/app/key_generator/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/app/key_generator/app/' : '/',
   outputDir: "app",
 },
 )
