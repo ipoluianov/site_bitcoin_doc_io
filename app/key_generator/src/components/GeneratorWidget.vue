1de22b26c01e95c99e81b0c60382ae9d583dce01
Ivan Poluianov <ivan.poluianov@gmail.com>
2024-01-20 12:04:13 +0100

    app

--- app/key_generator/src/components/GeneratorWidget.vue
+++ app/key_generator/src/components/GeneratorWidget.vue
@@ -1,19 +1,120 @@
 <template>
-    <div>
-        <button @click="generate">GENERATE</button>
+    <div style="text-align: left; display: flex; flex-direction: column; !max-width: 300px;">
+        <div class="block">
+            <button @click="generate">GENERATE</button>
+        </div>
+        <div class="block">
+            <div class="header">Address - BECH32</div>
+            <div class="content">
+                <pre>{{ addr_bech32 }}</pre>
+            </div>
+        </div>
+
+        <div class="block">
+            <div class="header">Address - P2PKH</div>
+            <div class="content">
+                <pre>{{ addr_p2pkh }}</pre>
+            </div>
+        </div>
+
+        <div class="block">
+            <div class="header">Private Key (WIF)</div>
+            <div class="content">
+                <pre>{{ privateKeyUc }}</pre>
+            </div>
+        </div>
+
+        <div class="block">
+            <div class="header-small">privateKeyC</div>
+            <div class="content-small">
+                <pre>{{ privateKeyC }}</pre>
+            </div>
+        </div>
+
+        <div class="block">
+            <div class="header-small">Public Key Compressed</div>
+            <div class="content-small">
+                {{ publicKeyC }}
+            </div>
+        </div>
+
+        <div class="block">
+            <div class="header-small">Address - P2PKH - HEX</div>
+            <div class="content-small">
+                <pre>{{ addr_p2pkh_hex }}</pre>
+            </div>
+        </div>
+
+
+        <div class="block">
+            <div class="header-small">addr_p2sh</div>
+            <div class="content-small">
+                <pre>{{ addr_p2sh }}</pre>
+            </div>
+        </div>
     </div>
 </template>
 
 
 <script>
 export default {
+    data() {
+        return {
+            privateKeyC: "",
+            privateKeyUc: "",
+            publicKeyC: "",
+            publicKeyUc: "",
+            addr_p2pkh_hex: "",
+            addr_p2pkh: "",
+            addr_bech32: "",
+            addr_p2sh: "",
+        };
+    },
     methods: {
-        generate() {
+        async generate() {
             console.log("123");
-        }
+            try {
+                const response = await fetch('http://x01.gazer.cloud:8488/api/bitcoin/generate_keys',);
+                const json = await response.json();
+                console.log(json)
+
+                this.privateKeyC = json.private_key_c;
+                this.privateKeyUc = json.private_key_u;
+                this.publicKeyC = json.public_key_compressed;
+                this.publicKeyUc = json.public_key_uncompressed;
+                this.addr_p2pkh_hex = json.address_p2pkh_hex;
+                this.addr_p2pkh = json.address_p2pkh;
+                this.addr_bech32 = json.address_bech32;
+                this.addr_p2sh = json.address_p2sh;
+            } catch (error) {
+
+                console.error(error);
+            }
+            console.log("---");
+        },
     }
 }
 </script>
 
-<style>
+<style scoped>
+.block {
+    max-width: 300px;
+}
+
+.header {
+    font-size: 24pt;
+}
+
+.content {
+    margin-bottom: 10px;
+    font-size: 14pt;
+}
+
+.header-small {
+    font-size: 16pt;
+}
+
+.content-small {
+    font-size: 10pt;
+}
 </style>
